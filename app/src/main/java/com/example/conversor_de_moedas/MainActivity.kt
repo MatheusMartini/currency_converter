df2f0b5576d38f2dae095760881fa3a6a8584304
MatheusMartini <matheus.martini@rede.ulbra.br>
2020-03-26 21:15:11 -0300

    first app in kotlin

---
+++ app/src/main/java/com/example/conversor_de_moedas/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.conversor_de_moedas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        btnChange.setOnClickListener(View.OnClickListener {
+            var valor = edtValor.text.toString().toDouble()
+            var conversao = edtConversao.text.toString().toDouble()
+
+            var total = valor / conversao
+
+            txtTotal.text = total.toString()
+        })
+    }
+}
